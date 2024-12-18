ce32f4f605b219a30f0e9e03e4716b822a6b6211
Coldhands <iamigorspiridonov@gmail.com>
2024-12-18 11:15:26 +0300

    №3Reworked main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@ int main(){
 	int massive[size]{5,1,2,6,1};
 	
 	int i_min{}, i_max{};
-
+	i_min = min_index(massive,size);
+	i_max = max_index(massive,size);
 	std :: cout << "i_min: " << i_min << "\ti_max: " << i_max << '\n';
 	
 	
 	int _sum{};
 	int begin{}, end{};
-
+	begin = min(i_max,i_min);
+	end = max(i_max,i_min);
+	_sum = sum(massive,size,begin, end);
 	std :: cout << "sum: " << _sum << '\n';
 	
 	return 0;
